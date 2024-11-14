6eb78e837b349e9adb7079dc53cd3997ee1058cc
Sunhaihua1 <2660245132@qq.com>
2024-11-14 23:58:39 +0800

    fix: C-l in lazyvim 13

---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      terminal = {
+        win = {
+          keys = {
+            nav_l = false,
+            nav_k = false,
+          },
+        },
+      },
+    },
+  },
+}
